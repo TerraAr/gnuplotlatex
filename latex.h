11f1e517b81abcd97bd19f94970d22e33501851e
TerraAr <carames@ufpr.br>
2020-09-08 22:41:22 -0300

    Adicionado contato.
     * Adicionado contato junto ao aviso de direitos autorais no início
       dos arquivos.

--- latex.h
+++ latex.h
@@ -1,4 +1,5 @@
-/* Copyright (C) 2019 - 2020 Fernando José Carames Vecino.
+/* Copyright (C) 2019 - 2020 Fernando José Carames Vecino 
+ * (fernando.carames@gmail.com).
  ************************************************************************
  *
  * This program is free software: you can redistribute it and/or modify
